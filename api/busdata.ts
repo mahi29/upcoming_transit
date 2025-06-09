6f019301e162a6ec7a40bde9a14601e3d5e43034
Mahith Amancherla <mahi.aman.29@gmail.com>
2025-06-08 21:36:33 -0400

    Get rid of express.js syntax

--- api/busdata.ts
+++ api/busdata.ts
@@ -1,13 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-    const url = `https://bustime.mta.info/api/siri/stop-monitoring.json?key=${process.env.MTA_API_KEY}&OperatorRef=MTA&MonitoringRef=308208&LineRef=MTA%20NYCT_B63`;
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        res.status(200).json(data);
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
-    }
+export default function handler(req: VercelRequest, res: VercelResponse) {
+    res.status(200).json({ message: 'Hello from Serverless Function!' });
 }
